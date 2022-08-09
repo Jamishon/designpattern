6b394b770d2f547ef1a0db91f8fab0dd9af53fed
jamishon <jamishon@126.com>
2022-08-09 23:14:36 +0800

    Add composite
    Signed-off-by: jamishon <jamishon@126.com>

---
+++ structural/composite.cc
@@ -0,0 +1,141 @@
+/**
+ * @file composite.cc
+ * @author Jamishon
+ * @brief 4 Roles: Component, Leaf, Composite, Client.
+ *        Objects are organized in to a tree-like structure,
+ *        and whloe-part objects have the consistent actions.
+ * @version 0.1
+ * @date 2022-08-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class Component {
+ public:
+  Component(std::string s) : id(s), parent(NULL), children(NULL) {}
+
+  virtual void Operation() { std::cout << id << "--Operation->" << std::endl; }
+
+  virtual int Add(Component* pc) { return -1; }
+
+  virtual int Remove(Component* pc) { return -1; }
+
+  virtual Component* GetChild(int i) { return NULL; }
+
+  virtual Component* GetComposite() { return NULL; }
+
+  virtual void SetParent(Component* p) { parent = p; }
+
+  virtual ~Component() {}
+
+ protected:
+  Component* parent;
+  std::vector<Component*>* children;
+  std::string id;
+};
+
+class Composite : public Component {
+ public:
+  Composite(std::string id) : Component(id) {
+    children = new std::vector<Component*>();
+  }
+
+  virtual int Add(Component* pc) {
+    children->push_back(pc);
+    pc->SetParent(this);
+    return children->size() - 1;
+  }
+
+  virtual int Remove(Component* pc) {
+    int i = 0;
+    for (std::vector<Component*>::iterator it = children->begin();
+         it != children->end(); it++, i++) {
+      if (*it == pc) {
+        pc->SetParent(NULL);
+        children->erase(it);
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  virtual Component* GetChild(int i) {
+    if (i < children->size())
+      return children->at(i);
+    else
+      return NULL;
+  }
+
+  virtual Composite* GetComposite() { return this; }
+
+  virtual void Operation() {
+    std::cout << "Composite:";
+    Component::Operation();
+  }
+};
+
+class Leaf : public Component {
+ public:
+  Leaf(std::string id) : Component(id) {}
+
+  virtual void Operation() {
+    std::cout << "Leaf:";
+    Component::Operation();
+  }
+};
+
+void PostOrderTraversalRecursive(Component* root) {
+  if (root) {
+    int index = 0;
+    if (root->GetComposite()) {
+      Component* pc = NULL;
+      while (pc = root->GetChild(index)) {
+        PostOrderTraversalRecursive(pc);
+        index++;
+      }
+    }
+    root->Operation();
+  }
+}
+
+void PreOrderTraversalRecursive(Component* root) {
+  if (root) {
+    root->Operation();
+    if (root->GetComposite()) {
+      int index = 0;
+      Component* pc = NULL;
+      while (pc = root->GetChild(index)) {
+        PreOrderTraversalRecursive(pc);
+        index++;
+      }
+    }
+  }
+}
+
+
+
+int main() {
+  Component* root = new Composite("root a");
+  root->Add(new Leaf("leaf b"));
+  root->Add(new Leaf("leaf c"));
+
+  Component* pc = new Composite("composite d");
+  root->Add(pc);
+
+  root->Add(new Leaf("leaf e"));
+
+  pc->Add(new Leaf("leaf f"));
+  pc->Add(new Leaf("leaf g"));
+  pc->Add(new Leaf("leaf h"));
+
+  PreOrderTraversalRecursive(root);
+  std::cout << std::endl;
+  PostOrderTraversalRecursive(root);
+  std::cout << std::endl;
+
+  
+}
